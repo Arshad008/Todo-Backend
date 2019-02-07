28f0fa95b608f4bc1e8a54d64deed2aeb7953e8f
arshad008 <arshadrauff008@gmail.com>
2019-02-07 14:39:05 +0530

    use body-parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const data = require('./data');
 const appConfig = require('./appConfig');
 
@@ -13,6 +14,7 @@ console.info("- Initializing Database");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 console.info("Initializing express");
 
 // Enable downloadable contents
