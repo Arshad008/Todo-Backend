6013a66860de7d7f27e7d44a84732068a517fabc
arshad008 <arshadrauff008@gmail.com>
2019-02-07 11:36:33 +0530

    implement create todo service

---
+++ services/Todo.js
@@ -0,0 +1,28 @@
+var Joi = require('Joi');
+const {
+    model, 
+    validationSchema
+} = require ('../data/model/Todo');
+const {
+    jsonHeader,
+    successResponse,
+    invalidParameterResponse,
+    errorResponse
+} = require('../responseHelper');
+
+module.exports = {
+    create: function(req,res){
+        jsonHeader(res);
+
+        let {error, value} = Joi.validate(req.body, validationSchema);
+        if(error){
+            invalidParameterResponse({}, error.message, res);
+            return;
+        }
+
+        let newTodo = new model(values);
+        newTodo.save()
+        .then((result)=>successResponse(result, res))
+        .catch(err=>errorResponse({}, err.message, res));
+    }
+}
