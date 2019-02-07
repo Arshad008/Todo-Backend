31826fde6f61011673ec04d13efb71c2b7e758b1
arshad008 <arshadrauff008@gmail.com>
2019-02-07 14:53:45 +0530

    response helper header changes

--- responseHelper.js
+++ responseHelper.js
@@ -1,8 +1,8 @@
 const CODE_SUCCESS = 1;
 const CODE_ERROR = 0;
 
-module.exports = {
-    jsonHeader:(res)=>res.setHeader('Content-Type',"application/json"),
+module.exports = {    
+    jsonHeader:(res)=>res.setHeader('Content-Type', 'application/json'),
     successResponse:(result,res)=>res.send(
         JSON.stringify(
             {
